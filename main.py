3cb34fc55e832bcce23975d669d2e72d528f53ab
attachemd <mattache.vivace@gmail.com>
2022-02-01 11:47:30 +0100

    Finished the CRUD.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import List
 from uuid import uuid4, UUID
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from models import User, Gender, Role
+from models import User, Gender, Role, UserUpdateRequest
 
 app = FastAPI()
 
@@ -41,3 +41,34 @@ async def fetch_users():
 async def register_user(user: User):
     db.append(user)
     return {'id': user.id}
+
+
+@app.delete('/api/v1/users/{user_id}')
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return f'user with id {user_id} has been deleted.'
+    raise HTTPException(
+        status_code=404,
+        detail=f'user with id {user_id} dose not exists.'
+    )
+
+
+@app.put('/api/v1/users/{user_id}')
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return f'user with id {user_id} has been updated.'
+        raise HTTPException(
+            status_code=404,
+            detail=f'user with id {user_id} dose not exists.'
+        )
